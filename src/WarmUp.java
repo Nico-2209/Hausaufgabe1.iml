e55c3fb3b46bc5628ac278073140e497a348eb9b
Nico-2209 <121798231+Nico-2209@users.noreply.github.com>
2023-03-27 09:49:47 +0200

    Test

---
+++ src/WarmUp.java
@@ -0,0 +1,48 @@
+public class WarmUp {
+
+    // Methode zur Überprüfung, ob eine Zahl eine Primzahl ist
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Methode zur Berechnung der Quersumme einer Zahl
+    public static int getDigitSum(int number) {
+        int sum = 0;
+
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+
+
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        System.out.println(isPrime(97));
+
+
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i) && isPrime(getDigitSum(i))) {
+                count++;
+            }
+        }
+
+        //System.out.println("Fuer " + count + " Zahlen x zwischen 1 und 100 gilt: x ist eine Primzahl und gleichzeitig ist die Quersumme von x ebenfalls eine Primzahl.");
+    }
+
+}
+
