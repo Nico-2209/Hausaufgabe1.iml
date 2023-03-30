76299e6cb91c597129e6b1fb5c8eacce8335f50b
Nico-2209 <121798231+Nico-2209@users.noreply.github.com>
2023-03-30 20:37:24 +0200

    Junit Tests

--- src/WarmUp.java
+++ src/WarmUp.java
@@ -5,7 +5,6 @@ public class WarmUp {
         if (number <= 1) {
             return false;
         }
-//test
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
