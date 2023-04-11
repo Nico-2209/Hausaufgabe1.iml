739a7c7ad25e3892187b608b7384a7b113e336f5
Nico-2209 <nico-2018@web.de>
2023-04-11 17:04:56 +0200

    Create Aufgaben.java

---
+++ src/Aufgaben/ZsmDatentypen/Aufgaben.java
@@ -0,0 +1,17 @@
+package Aufgaben.ZsmDatentypen;
+
+public class Aufgaben {
+//Übungsaufgabe: zusammengesetzte Datentypen verwenden:
+
+//1.Erstellen Sie ein Programm, das ein Array aus sechs String-Variablen erzeugt.
+//Die-ses soll die Vornamen der Teilnehmer an einem Sprachkurs enthalten. Geben Sie das gesamte Array sowie den ersten und den letzten Namen der Liste aus.
+// Ändern Sie danach einen beliebigen Eintrag und geben Sie das Array erneut aus.
+
+
+
+
+
+
+
+
+}
